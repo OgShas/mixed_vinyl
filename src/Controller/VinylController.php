ec128ffa0ac7d4b0f89400072a140e1d2964f68a
Alexander Genov <shasog@gmail.com>
2023-03-27 15:37:15 +0300

    the 3

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -2,16 +2,40 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use function Symfony\Component\String\u;
 
-class VinylController
+class VinylController extends AbstractController
 {
     #[Route('/')]
     public function homePage() : Response {
 
-        return new Response('The Response');
+        $tracks=[
+            ['song'=>'Gangsta\'s Paradise','artist'=>'Coolio'],
+            ['song'=>' Waterfalls','artist'=>'TLC'],
+            ['song'=>'Creep','artist'=>'Radiohead'],
+            ['song'=>'Show me how to live','artist'=>'Audioslave'],
+            ['song'=>'Nobodies','artist'=>'Marilyn Manson'],
+            ['song'=>'Simple man','artist'=>'Lynyrd Skynyrd'],
+
+        ];
+
+        $tracks2=[
+            ['song'=>'Bunker','artist'=>'Balthazar'],
+            ['song'=>'Hero','artist'=>'Foo Fighters'],
+            ['song'=>'Age of Machine','artist'=>'Greta Van Fleet'],
+            ['song'=>'High and Dry','artist'=>'Radiohead'],
+            ['song'=>'I Know','artist'=>'Placebo'],
+
+        ];
+
+        return $this->render("vinyl/homepage.html.twig",
+            ['title'=>'PB & Jams',
+                'tracks'=>$tracks,
+                'tracks2'=>$tracks2,
+                ]);
     }
 
     #[Route('/browse/{slug}')]
