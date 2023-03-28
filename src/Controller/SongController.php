e831128f898898a723a7fbe73f6251ec24e7b1e8
Alexander Genov <shasog@gmail.com>
2023-03-28 14:04:40 +0300

    the last

--- src/Controller/SongController.php
+++ src/Controller/SongController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,12 +11,13 @@ use Symfony\Component\Routing\Annotation\Route;
 class SongController extends AbstractController
 {
     #[Route('/api/songs/{id<\d+>}', methods:['GET'])]
-    public function getSong(int $id) : Response
+    public function getSong(int $id,LoggerInterface $logger) : Response
     {
            $song=['id'=>$id,
           'name'=>'Creep',
           'url'=>'https://youtu.be/XFkzRNyygfk'];
 
+           $logger->info('returning API response for song {song}',['song'=>$id,]);
 
            return $this->json($song);
     }
