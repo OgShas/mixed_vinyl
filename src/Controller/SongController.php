80ba5e812846d807425b33fa31de7d36634bc4ae
Alexander Genov <shasog@gmail.com>
2023-03-29 11:04:08 +0300

    29.03

--- src/Controller/SongController.php
+++ src/Controller/SongController.php
@@ -10,7 +10,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class SongController extends AbstractController
 {
-    #[Route('/api/songs/{id<\d+>}', methods:['GET'])]
+    #[Route('/api/songs/{', methods:['GET'])]
     public function getSong(int $id,LoggerInterface $logger) : Response
     {
            $song=['id'=>$id,
